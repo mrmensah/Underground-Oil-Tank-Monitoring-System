3987d7dbe92d6cb2d4a42d9eda59316980416cc9
mrmensah <tbmensah98@gmail.com>
2021-08-18 20:48:00 +0000

    django down

--- Application/backend/monitor/models.py
+++ Application/backend/monitor/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Data(models.Model):
+    timestamp = models.TimeField(auto_now_add=True)
+    temperature = models.FloatField()
+    level = models.FloatField()
+
+    class meta:
+        ordering = ['created']
---
+++ Application/backend/monitor/serializers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+from monitor.models import Data
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email']
+
+class DataSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Data
+        fields =['temperature', 'level']
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
